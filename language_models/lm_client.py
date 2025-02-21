c1fb6ea6e74c80afe62f11e12df4c928fc8c5318
alexl6 <62918876+alexl6@users.noreply.github.com>
2025-02-21 15:10:16 -0800

    Update language model client

--- language_models/lm_client.py
+++ language_models/lm_client.py
@@ -1,14 +1,15 @@
 from ollama import Client
 from ollama import ChatResponse
+import sys
 
 client: ChatResponse = Client(
   host='http://fruit.alexluo.com:11434',
   headers={'x-some-header': 'some-value'}
 )
 
-def query_lm(query: str='Why is the sky blue?'):
+def query_lm(query: str='Who are you?'):
     response = client.chat(
-        model='deepseek-r1:14b',
+        model='qwen2.5:14b',
         messages=[{
             'role': 'user',
             'content': query,
@@ -18,7 +19,12 @@ def query_lm(query: str='Why is the sky blue?'):
     
     for chunk in response:
         print(chunk['message']['content'], end='', flush=True)
+    print()
 
 # print(response.message.content)
 if __name__ == '__main__':
-    query_lm()
+    client.pull('qwen2.5:14b')
+    if len(sys.argv) > 1:
+        query_lm(" ".join(sys.argv[1:]))
+    else:
+        query_lm()
